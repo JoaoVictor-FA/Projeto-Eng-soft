ef80d940e0d0287b9c549f656a30772b136fa970
Lávio <laviovale@gmail.com>
2022-10-30 12:00:27 -0300

    feat: add password visibility

--- frontend-engsoft/src/components/Login.tsx
+++ frontend-engsoft/src/components/Login.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import eye from "../assets/Eye.svg";
+
 export default function Login() {
+  const [visiblePassword, setVisiblePassword] = useState(false);
+
   return (
     <div className="flex flex-col items-center h-max w-70 bg-blue-darker text-white p-4 gap-4 font-bold rounded shadow-xl shadow-gray-main ">
       <h1 className="text-lg">Faça o login para acessar</h1>
@@ -13,12 +18,18 @@ export default function Login() {
         </label>
       </div>
       <div>
-        <label className="flex flex-col">
+        <label className="flex flex-col relative">
           Senha
           <input
-            type="password"
+            type={visiblePassword ? "text" : "password"}
             name="password"
             className="bg-gray-dark h-10 border-blue-main border-2 rounded focus:outline-none p-2"
+          />
+          <img
+            src={eye}
+            alt="eye"
+            className="absolute right-2 top-7 cursor-pointer"
+            onClick={() => setVisiblePassword(!visiblePassword)}
           />
         </label>
       </div>
