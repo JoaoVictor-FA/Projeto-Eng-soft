96667b0b639327201ba1dfe39fac38839a7de8e7
Joao Pedro Fernandes <joaofernandesufba@gmail.com>
2022-11-03 21:36:51 -0300

    Adding font and router, starting student homepage

--- frontend-engsoft/src/App.tsx
+++ frontend-engsoft/src/App.tsx
@@ -1,10 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
 import Login from './components/Login'
-
+import StudentHome from './pages/StudentHome'
 function App() {
 
   return (
     <div className='bg-gradient-to-br from-black to-blue-pure h-screen flex justify-center items-center'>
-      <Login />
+    <Routes>
+      <Route path='/' element={<Login />}/>
+      <Route path='/Student' element={<StudentHome />} />
+    </Routes>
     </div>
   )
 }
---
+++ frontend-engsoft/src/pages/StudentHome.tsx
@@ -0,0 +1,17 @@
+export default function StudentHome() {
+  return (
+    <main className="text-white">
+      <h1 className="font-bold text-lg">Olá Lorem Ipsum</h1>
+      <section>
+        <h3>Suas Tarefas</h3>
+        <div>
+
+        </div>
+      </section>
+
+      <section>
+        <h3>Inscreva-se numa atividade</h3>
+      </section>
+    </main>
+  )
+}
