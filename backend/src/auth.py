10a0bfb016aa3040ed39f4181ff704c3c20b71ce
Edyo <edyoandrade@gmail.com>
2022-11-28 18:37:36 -0300

    endpoint de registro funcionando

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -1,9 +1,20 @@
 from flask import Blueprint
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from .models import Usuario
 
 auth = Blueprint('auth', __name__)
 api = Api(auth)
+
+def email(email_str: str):
+    if email_str.find('@') != -1:
+        if (Usuario.query.filter_by(email=email_str).first()): # checa se já existe na db
+            raise ValueError(f'{email_str} já foi registrado')
+        else:
+            return email_str
+    else:
+        raise ValueError(f'{email_str} não é um email válido')
 
 class Login(Resource):
     def get(self):
@@ -11,9 +22,36 @@ class Login(Resource):
     
 api.add_resource(Login, '/login')
 
+
+# configuração do parser de registro
+rParser = reqparse.RequestParser()
+rParser.add_argument( # nome
+    'nome', dest='nome',
+    location='form', required=True,
+    help='Nome do usuário'
+)
+
+rParser.add_argument( # email
+    'email', dest='email', type=email,
+    location='form', required=True,
+)
+
+rParser.add_argument( # senha
+    'password', dest='password',
+    location='form', required=True,
+    help='Senha do usuário'
+)
+
 class Registrar(Resource):
-    def get(self):
-        return 'Registrar'
+    def post(self):
+        args = rParser.parse_args()
+        
+        usuario = Usuario(email=args.email, nome=args.nome, password=generate_password_hash(args.password, method='sha256'))
+        
+        db.session.add(usuario)
+        db.session.commit()
+        
+        return 'Usuário Cadastrado'
     
 api.add_resource(Registrar, '/registrar')
 
