fc6e9c780606dfc1f47f2a551d4367ef1bba2132
pavaroStack <71470927+pavaroStack@users.noreply.github.com>
2022-12-01 18:32:27 -0300

    commit da tabela de Aluno

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,4 +1,5 @@
 from flask_login import UserMixin
+from sqlalchemy import ForeignKey
 from . import db
 
 class Usuario(UserMixin, db.Model):
@@ -6,3 +7,19 @@ class Usuario(UserMixin, db.Model):
     nome = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+    type = db.Column(db.String(100), nullable=False)
+
+    __mapper_args__ = {
+        "polymorphic_identity": "usuario",
+        "polymorphic_on": "type",
+    }
+
+class Aluno(Usuario):
+    __tablename__ = "aluno"
+    id = db.Column(db.ForeignKey('usuario.id'), primary_key=True)
+    tarefas = db.relationship("Tarefas")
+    status = db.relationship("Status")
+
+    __mapper_args__ = {
+        "polymorphic_identity": "aluno",
+    }
