7e5f8c03f276d1da25cfbcffc3206c9b3d8a82e8
Edyo <edyoandrade@gmail.com>
2022-12-02 15:38:28 -0300

    adiciona o resto de Atividade

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,3 +1,6 @@
+import datetime
+import random
+import string
 from flask_login import UserMixin
 from . import db
 
@@ -39,9 +42,7 @@ class Aluno(Usuario):
     
         return {
             "status": status,
-            "tarefa": {
-                "id": item.id
-            }
+            "tarefa": item()
         }
     
     def __call__(self):
@@ -56,7 +57,7 @@ class Aluno(Usuario):
         
 class Professor(Usuario):
     id = db.Column(db.ForeignKey(Usuario.id), primary_key=True)
-    atividades = db.relationship("Atividade", back_populates="autor", cascade='all, delete-orphan', lazy="joined")
+    atividades = db.relationship("Atividade", cascade='all, delete-orphan', lazy="joined")
     __mapper_args__ = {'polymorphic_identity': 'professor'}
     
     def __call__(self):
@@ -65,15 +66,37 @@ class Professor(Usuario):
                 "nome": self.nome,
                 "email": self.email,
                 "tipo": self.type,
-                "atividades": [{
-                        "id": item.id
-                    } for item in self.atividades]
+                "atividades": [item() for item in self.atividades]
             }
         }
     
     
+# gera codigo de 6 caracteres formado por letras maiusculas e numeros para Atividade
+def geraCodigo(size = 6, chars = string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 class Atividade(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100))
     autor_id = db.Column(db.ForeignKey(Professor.id))
-    autor = db.relationship("Professor", back_populates="atividades", uselist=False)
+    dataAbertura = db.Column(db.DateTime(), default=datetime.datetime.now())
+    dataLimite = db.Column(db.DateTime())
+    #descricao
+    # respostas
+    codigoAcesso = db.Column(db.String(6), default=geraCodigo())
     alunos = db.relationship("Aluno", back_populates="tarefas", secondary=association_table, lazy="joined")
+    complexidade = db.Column(db.String(10))
+    
+    def __call__(self):
+        return {
+            "data": {
+                "nome": self.nome,
+                "data de abertura": self.dataAbertura.strftime("%d/%m/%Y, %H:%M:%S"),
+                "data limite": self.dataLimite.strftime("%d/%m/%Y, %H:%M:%S"),
+                # descricao
+                # respostas
+                "codigo de acesso": self.codigoAcesso,
+                "alunos": [{"nome": item.nome, "email": item.email} for item in self.alunos],
+                "complexidade": self.complexidade
+            }
+        }
